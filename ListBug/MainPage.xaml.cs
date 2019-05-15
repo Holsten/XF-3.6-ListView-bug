da6613e67c97ab2e13f08d3de4500e3777420ad2
Fredrik Holstensson <fholstensson@betomorrow.com>
2019-05-15 17:13:17 +0200

    First commit

---
+++ ListBug/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ListBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<object> Items { get; private set; }
+        public MainPage()
+        {
+            Items = new List<object>();
+            for (int i = 0; i < 10; i++)
+            {
+                Items.Add("item " + i);
+            }
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+    }
+}
